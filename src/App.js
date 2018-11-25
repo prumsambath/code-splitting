307c192befe285f7ab4e8677adf7b0ceaaf8975e
Sambath Prum <psb.sambath@gmail.com>
2018-11-25 05:37:40 -0600

    lazy-load page 2 and 3 - code splitting

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import Page1 from './Page1';
-import Page2 from './Page2';
-import Page3 from './Page3';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       route: 'page1',
+      component: null,
     };
   }
 
   onRouteChange = route => {
-    this.setState({ route });
+    if (route === 'page1') {
+      this.setState({ route });
+    } else if (route === 'page2') {
+      import('./Page2').then(Page2 => {
+        this.setState({ route, component: Page2.default });
+      });
+    } else if (route === 'page3') {
+      import('./Page3').then(Page3 => {
+        this.setState({ route, component: Page3.default });
+      });
+    }
   };
 
   render() {
     let { route } = this.state;
     if (route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />;
-    } else if (route === 'page2') {
-      return <Page2 onRouteChange={this.onRouteChange} />;
-    } else if (route === 'page3') {
-      return <Page3 onRouteChange={this.onRouteChange} />;
+    } else {
+      return <this.state.component onRouteChange={this.onRouteChange} />;
     }
   }
 }

123dd62504bfe8db5e5ed96cd7fe6ca77e9f17a7
Sambath Prum <psb.sambath@gmail.com>
2018-11-25 23:08:32 -0600

    Use HOC for code splitting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Page1 from './Page1';
+import AsyncComponent from './AsyncComponent';
 
 class App extends Component {
   constructor() {
@@ -12,25 +13,19 @@ class App extends Component {
   }
 
   onRouteChange = route => {
-    if (route === 'page1') {
-      this.setState({ route });
-    } else if (route === 'page2') {
-      import('./Page2').then(Page2 => {
-        this.setState({ route, component: Page2.default });
-      });
-    } else if (route === 'page3') {
-      import('./Page3').then(Page3 => {
-        this.setState({ route, component: Page3.default });
-      });
-    }
+    this.setState({ route });
   };
 
   render() {
     let { route } = this.state;
     if (route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />;
-    } else {
-      return <this.state.component onRouteChange={this.onRouteChange} />;
+    } else if (route === 'page2') {
+      const AsyncPage2 = AsyncComponent(() => import('./Page2'));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />;
+    } else if (route === 'page3') {
+      const AsyncPage3 = AsyncComponent(() => import('./Page3'));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />;
     }
   }
 }
